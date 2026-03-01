90952c02c7ac54b18a10aed0bbe2549389c78adf
chetan <chetanchoudhary435@gmail.com>
2026-03-02 01:33:36 +0530

    backend complete

--- middelware/checkAuth.js
+++ middelware/checkAuth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 
 const checkAuthMiddelware = async (req, res, next) => {
@@ -15,7 +16,15 @@ const checkAuthMiddelware = async (req, res, next) => {
     try {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token - user not found",
+            });
+        }
+        req.user=decoded;
+
         next();
         
     } catch (error) {
